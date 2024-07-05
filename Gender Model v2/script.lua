61c98741303da12ee1b576202f589b484bd9ccf8
Skye Pearl <rainblueskylar@gmail.com>
2024-07-05 02:01:27 -0700

    a

--- Gender Model v2/script.lua
+++ Gender Model v2/script.lua
@@ -4,7 +4,7 @@
 vanilla_model.ARMOR:setVisible(false)
 
 --hide vanilla cape model
-vanilla_model.PLAYER:setVisible(false)
+-- vanilla_model.PLAYER:setVisible(false)
 vanilla_model.CAPE:setVisible(false)
 models.model:setPrimaryTexture("SKIN")
 
